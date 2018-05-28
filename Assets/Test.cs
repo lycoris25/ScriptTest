866492dfc58e99185e0b1ff6d50cc38c0c99e4cf
lycoris25 <lycoris8460>
2018-05-28 12:15:22 +0900

    ScriptTest Lessen4 課題提出
    配列の出力、Bossクラスの変数mpとMagic関数を作成しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = { 3, 17, 29, 31, 47 };
+
+	
+		for (int i = 4; i >= 0; i--) {
+			Debug.Log (array [i]);
+		}
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/boss1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+	private int mp = 53;
+
+	public void Magic(int mp){
+		if (mp > 5) {
+			this.mp -= 5;
+			Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+		} else {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+		}
+	}
+}
+
+public class boss1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Boss lastboss = new Boss ();
+
+		lastboss.Magic(3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
