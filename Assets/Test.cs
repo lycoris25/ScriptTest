4e00f722ba425efde9adc9b0b777f1df134bf592
lycoris25 <lycoris8460>
2018-05-28 18:12:53 +0900

    ScriptTest Lessen4再提出
    配列を順番に表示したのち逆順に表示するようにしました。Magic関数を11回呼ばれる処理を追加しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,13 +7,13 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		int[] array = { 3, 17, 29, 31, 47 };
-
-	
-		for (int i = 4; i >= 0; i--) {
+		for (int i = 0; i < array.Length; i++) {
 			Debug.Log (array [i]);
 		}
-			
-	}
+		for (int i = 4; i >= 0; i--) {
+			Debug.Log (array [i]);
+				}
+			}
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/boss1.cs
+++ Assets/boss1.cs
@@ -6,7 +6,7 @@ public class Boss {
 	private int mp = 53;
 
 	public void Magic(int mp){
-		if (mp > 5) {
+		if (this.mp > 5) {
 			this.mp -= 5;
 			Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
 		} else {
@@ -20,8 +20,9 @@ public class boss1 : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Boss lastboss = new Boss ();
-
-		lastboss.Magic(3);
+		for (int i = 0; i < 11; i++) {
+			lastboss.Magic (53);
+		}
 	}
 	
 	// Update is called once per frame
